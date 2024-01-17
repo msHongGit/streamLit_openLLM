cd94888403d79cbff2811413d92d3f49c943904f
MinsungHong <minsung.holdtime@gmail.com>
2024-01-17 17:28:57 +0900

    Commit

--- streamlit_refer_openLLM.py
+++ streamlit_refer_openLLM.py
@@ -203,7 +203,7 @@ def get_conversation_chain(vetorestore):
 
     # Create llm chain    
     # llm_chain = HuggingFaceHub(repo_id="kyujinpy/Ko-PlatYi-6B", model_kwargs={"temperature":0.1, "max_length":300})
-    llm_chain = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":300})
+    llm_chain = HuggingFaceHub(repo_id="JaeyeonKang/CCK-v1.0.0-DPO", model_kwargs={"temperature":0.5, "max_length":300})
     logger.debug("Load HF-LLM model")
 
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer')
